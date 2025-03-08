09eb4edc25b39556269b0e0523682d3e6428f1ae
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2025-03-08 13:50:20 +0530

    add home page and truck animation in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Home from './component/Home'
 import Navbar from './component/NavBar'
+import Services from './component/Services'
 
 function App() {
 
   return (
 <>
 <Navbar/>
+<Home/>
+<Services/>
 </>      
   )
 }
