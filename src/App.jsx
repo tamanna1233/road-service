99ceae9e0c1d76b3a8ebc9eeae100f7a834c174b
TAMANNA <tamannakarthol320@gmail.com>
2025-03-10 10:41:03 +0530

    truck animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
+import EmergencyBadge from './component/EmergencyBadge'
 import Navbar from './component/NavBar'
+import ServiceCardsRow from './component/Services'
+import TruckAnimation from './component/TruckAnimation'
 
 function App() {
 
   return (
 <>
+<div className='bg-black'>
 <Navbar/>
+<EmergencyBadge/>
+<TruckAnimation/>
+<ServiceCardsRow/>
+</div>
 </>      
   )
 }
---
+++ src/component/EmergencyBadge.jsx
@@ -0,0 +1,11 @@
+const EmergencyBadge = () => {
+    return (
+      <div className="inline-flex items-center gap-2 p-2 px-4 bg-black bg-opacity-50 rounded-full text-sm text-white">
+        <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></div>
+        <span>Available 24/7 For Emergency Roadside Assistance</span>
+      </div>
+    );
+  };
+  
+  export default EmergencyBadge;
+  
