622277406ce654786d9d84256d8ff0b0132514f4
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2025-04-28 09:43:59 +0530

    add new logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,17 +27,17 @@ export default function Footer() {
   return (
     <>
       <footer className="  px-4 md:px-12 py-12 bg-black text-white border-t-2">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 ">
           {/* Company Info */}
           <div>
             <img
               src={logo}
               alt="brand logo"
-              className="h-24 mx-auto"
+              className="h-32 mx-auto"
               loading="lazy"
             />
 
-            <p className="px-2 md:px-8 text-xl py-2">
+            <p className="px-2 md:px-8 text-lg py-2">
               Your trusted partner in complete logistics, transportation, and
               fleet management services across North America since 2016.{" "}
             </p>
